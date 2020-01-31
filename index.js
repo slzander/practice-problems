0c569b06c52ba2b73d2bd9dcad1a07d460e1b89b
slzander <staceyzander@gmail.com>
2020-01-31 10:11:05 -0700

    solves array_diff problem

--- index.js
+++ index.js
@@ -7,4 +7,14 @@
 
 // array_diff([1,2,2,2,3],[2]) == [1,3]
 
-
+function array_diff(a, b) {
+    for (let i = 0; i < b.length; i++){
+      for(let j = 0; j < a.length; j++){
+        if(a[j] === b[i]){
+          a.splice(j, 1)
+          j = j-1
+        }
+      }
+    }
+    return a
+  }
