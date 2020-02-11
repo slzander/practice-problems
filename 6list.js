64192c070a6255c2046e2d8af80ed1418177fd2b
slzander <staceyzander@gmail.com>
2020-02-10 20:04:28 -0700

    solves list problem

--- 6list.js
+++ 6list.js
@@ -18,5 +18,15 @@
 // Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
 
 function list(names){
-    //your code here
-  }
+    if(names.length === 0){
+        return ''
+    } else if (names.length === 1){
+        return names[0].name
+    } else {
+        let listString = names[0].name
+        for(i = 1; i < names.length - 1; i++){
+            listString = listString + ', ' + names[i].name
+        }
+        return listString + ' & ' + names[names.length-1].name
+    }
+}
